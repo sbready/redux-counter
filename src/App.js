63082720935b21fd7aba486c2ea41ffee2392bcb
Steven Bready <stevenbready@MacBook-Pro.local>
2017-11-13 14:50:36 -0700

    completed mini project

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { increment, decrement } from './ducks/counter';
 
 import "./App.css";
 
 export class App extends Component {
 	render() {
+		console.log(this.props);
 		return (
 			<div className="app">
 				<section className="counter">
-					<h1 className="counter__current-value">{ 0 }</h1>
+					<h1 className="counter__current-value">{ this.props.currentValue }</h1>
 					<div className="counter__button-wrapper">
 						<button
 							className="counter__button increment-one"
-							onClick={ () => null }
+							onClick={ () => this.props.increment(1) }
 						>
 							+1
 						</button>
 						<button
 							className="counter__button increment-five"
-							onClick={ () => null }
+							onClick={ () => this.props.increment(5) }
 						>
 							+5
 						</button>
 						<button
 							className="counter__button decrement-one"
-							onClick={ () => null }
+							onClick={ () => this.props.decrement(1) }
 						>
 							-1
 						</button>
 						<button
 							className="counter__button decrement-five"
-							onClick={ () => null }
+							onClick={ () => this.props.decrement(5) }
 						>
 							-5
 						</button>
@@ -60,4 +63,16 @@ export class App extends Component {
 	}
 }
 
-export default App;
+function mapStateToProps(state) {
+	return state;
+}
+
+let actionCreators = {
+	increment: increment,
+	decrement: decrement
+}
+
+const decorator = connect( mapStateToProps, actionCreators );
+const decoratedComponent = decorator (App);
+
+export default decoratedComponent;
--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,38 @@
 // change this to true to see test results on the black diamond section.
 export const BLACK_DIAMOND = false;
+
+const initialState = {
+    currentValue: 0
+}
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+export function increment(amount) {
+    return {
+        type: INCREMENT,
+        amount: amount
+    }
+}
+
+export function decrement(amount) {
+    return {
+        type: DECREMENT,
+        amount: amount
+    }
+}
+
+export default function counter(state = initialState, action) {
+    switch (action.type) {
+        case INCREMENT:
+            return Object.assign(
+                {}, {currentValue: state.currentValue + action.amount}
+            )
+        case DECREMENT:
+            return Object.assign(
+                {}, {currentValue: state.currentValue - action.amount}
+        )
+        default:
+            return state;
+    }
+}
